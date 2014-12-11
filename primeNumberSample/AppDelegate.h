d185e49ad050a45805ed7770b78b6b014996f49b
Ryunosuke Abe <r.abe.dragon@gmail.com>
2014-12-11 11:08:42 +0900

    Initial Commit

---
+++ primeNumberSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  primeNumberSample
+//
+//  Created by 阿部 竜之介 on 2014/12/11.
+//  Copyright (c) 2014年 RyunosukeAbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ primeNumberSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  primeNumberSample
+//
+//  Created by 阿部 竜之介 on 2014/12/11.
+//  Copyright (c) 2014年 RyunosukeAbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
