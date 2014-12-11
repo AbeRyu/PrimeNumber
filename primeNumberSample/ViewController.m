a4087e1fdb1f054103f36b2f4e8f72f3d64d2bcd
Ryunosuke Abe <r.abe.dragon@gmail.com>
2014-12-11 11:28:52 +0900

    first commit

--- primeNumberSample/ViewController.m
+++ primeNumberSample/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self makePrimeShooter];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +27,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)makePrimeShooter{
+    
+    //画面取得
+    UIScreen *sc = [UIScreen mainScreen];
+    
+    //ステータスバー込みのサイズ
+    CGRect rect = sc.bounds;
+    NSLog(@"%.1f, %.1f", rect.size.width, rect.size.height);
+    
+    //ステータスバーを除いたサイズ
+    //    rect = sc.applicationFrame;
+    //    NSLog(@"%.1f, %.1f", rect.size.width, rect.size.height);
+    
+    
+    UIView *moveView =[[UIView alloc] initWithFrame:CGRectMake(rect.size.width/2-50,rect.size.height-200,100,100)];
+    moveView.backgroundColor =  [UIColor blackColor];
+    [self.view addSubview:moveView];
+    NSLog(@"%.1f, %.1f", moveView.center.x, moveView.center.y);
+    moveView.layer.cornerRadius = 50.0;
+    moveView.layer.borderColor = [[UIColor blueColor]CGColor];
+    moveView.layer.borderWidth = 5;
+    
+    
+    // UILabelを生成
+    UILabel *primeLabel = [[UILabel alloc] init];
+    primeLabel.frame = CGRectMake(moveView.frame.size.width/2-37.5, moveView.frame.size.height/2-37.5, 75, 75);
+    primeLabel.text = @"1994";
+    primeLabel.textAlignment = UITextAlignmentCenter;
+    //    primeLabel.backgroundColor = [UIColor redColor];
+    primeLabel.textColor = [UIColor whiteColor];
+    primeLabel.font = [UIFont boldSystemFontOfSize:30];
+    [moveView addSubview:primeLabel];
+}
 @end
